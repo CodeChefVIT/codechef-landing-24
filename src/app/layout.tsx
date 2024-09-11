0bff80d68aba2a00b5dc2e52c6ea9826bb7f4eeb
DhruvsChopra <dhruvsinghchopra70@gmail.com>
2024-09-11 11:57:10 +0530

    adding loader as a page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,20 @@
 "use client";
+
 import ReactLenis from "@studio-freight/react-lenis";
 import { Inter } from "next/font/google";
-import React from "react";
-
+import React, { useEffect, ReactNode } from "react";
+import { usePathname } from "next/navigation";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
-import Script from "next/script";
 import "../styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+type RootLayoutProps = {
+  children: ReactNode;
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
   const lenisOptions = {
     lerp: 5,
     duration: 1.2,
@@ -22,21 +22,41 @@ export default function RootLayout({
     smooth: true,
   };
 
+  const pathname = usePathname();
+  const isLoaderPage = pathname === "/loader";
+
+  useEffect(() => {
+    
+    const script1 = document.createElement("script");
+    script1.src = "/assets/navbar/js/demo1.js";
+    script1.onload = () => {
+     
+    };
+    document.body.appendChild(script1);
+
+    const script2 = document.createElement("script");
+    script2.src = "/assets/navbar/js/modernizr-2.6.2.min.js";
+    document.body.appendChild(script2);
+
+    const script3 = document.createElement("script");
+    script3.src = "/assets/navbar/js/polyfills.js";
+    document.body.appendChild(script3);
+
+    return () => {
+      
+      document.body.removeChild(script1);
+      document.body.removeChild(script2);
+      document.body.removeChild(script3);
+    };
+  }, []);
+
   return (
     <ReactLenis root options={lenisOptions}>
       <html lang="en">
-        <head>
-          <Script src="/assets/navbar/js/demo1.js" strategy="lazyOnload" />
-          <Script
-            src="/assets/navbar/js/modernizr-2.6.2.min.js"
-            strategy="lazyOnload"
-          />
-          <Script src="/assets/navbar/js/polyfills.js" strategy="lazyOnload" />
-        </head>
         <body className={`${inter.className} overflow-x-hidden w-screen`}>
           {children}
-          <Footer />
-          <Navbar />
+          {!isLoaderPage && <Footer />}
+          {!isLoaderPage && <Navbar />}
         </body>
       </html>
     </ReactLenis>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+'use client'
 import Events from "@/components/sections/Events";
+import Rive from "@rive-app/react-canvas";
 export default function Home() {
   return (
     <main>
       <Events />
+      <Rive
+      src="/websiteloader.riv"></Rive>
     </main>
   );
 }
