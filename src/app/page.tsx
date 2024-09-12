00c2f77e73712d1cc379203aa61688e2677850e6
DhruvsChopra <dhruvsinghchopra70@gmail.com>
2024-09-13 00:24:27 +0530

    Adding loader to event page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Events from "@/components/sections/Events";
-
+import ClientComponent from "@/components/ClientComponent";
+import Loader from "@/components/loader";
 const Home: React.FC = () => {
   return (
     <main>
+      <ClientComponent>
       <Events />
+      </ClientComponent>
+      
+      
     </main>
   );
 };
